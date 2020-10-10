74a302a4a51a408446c021f5ae3c47edc6dd8ec1
Ievgenii Meshcheriakov <eugen@debian.org>
2020-10-10 13:28:17 +0200

    Remove most mentions of 'us'
    I don't need this for my receiver.

--- lib/aishandler.dart
+++ lib/aishandler.dart
@@ -5,17 +5,18 @@ import 'package:nmea/nmea.dart';
 /// Core AIS handler class used to connect to an NMEA0183 source and
 /// decode messages therefrom.
 ///
-/// Override `we(PCS)` and `they(PCS us, PCS them, int mmsi)`
+/// Override `they(PCS them, int mmsi)`
 /// to implement your own AIS handler.
 ///
-/// `we(PCS)` will be invoked any time an incoming position record (RMC) is received
+/// `they(them, mmsi)` will be invoked any time a (complete) VDM sentence is
+/// received.
 ///
-/// `they(us, them, mmsi)` will be invoked any time a (complete) VDM sentence is received.
+/// This will attempt to reconnect if a connection is dropped see
+/// [NMEASocketReader] for details.
 ///
-/// This will attempt to reconnect if a connection is dropped see [NMEASocketReader] for details.
-///
-/// This class maintains a (non-persistent) cache of the most recent message of each type received, keyed by MMSI
-/// useful for doing things like name lookup and drilldown to full AIS data should you need to.
+/// This class maintains a (non-persistent) cache of the most recent message
+/// of each type received, keyed by MMSI useful for doing things like name
+/// lookup and drilldown to full AIS data should you need to.
 abstract class AISHandler {
   String _lastMsg;
   String _payload = '';
@@ -45,38 +46,15 @@ abstract class AISHandler {
   // The underlying NMEA reader
   NMEAReader get nmea => _nmea;
 
-  /// [we] will be invoked each time a position record (RMC) is received on the NMEA bus
-  void we(final PCS us);
-
   /// [they] will be invoked each time an AIS target is reported
-  ///
-  /// [us] is the [PCS] for our boat
   ///
   /// [them] is the [PCS] of the target
   ///
   /// [mmsi] is the MMSI of the target.
-  void they(final PCS us, final PCS them, final int mmsi);
-
-  // most recently received position
-  PCS _us;
-
-  // Map of MMSI to ship name
-  // Map<int,String> _names = {};
+  void they(final PCS them, final int mmsi);
 
   // Map of MMSI to Type to most recently received VDM
   Map<int,Map<int,AIS>> _static = Map();
-
-  /// Ship name of given mmsi or null if unknown.
-  ///
-  /// This is derived from Type5 and Type24 messages, and is not persistent
-  /// int this basic implementation, so you will only get non-null if a message of that
-  /// type has already been received during the current lifecycle of this class.
-  // String name(final int mmsi) => _names[mmsi];
-
-  /// Invoked when a message arrives that associates MMSI with a name
-  /// Useful to override if you want to track this (e.g to provide persistence capability)
-  // @mustCallSuper
-  // void set(final int mmsi, String name) => _names[mmsi] = name;
 
   /// Most recent message of given [type] from [mmsi]
   ///
@@ -93,19 +71,6 @@ abstract class AISHandler {
 
   // receiver for NMEA messges
   void _handleNMEA(var msg) {
-    if (msg is RMC) {
-      _us = new PCS(msg.position.lat, msg.position.lng, msg.trackMadeGood, msg.sog);
-      // print('   '+_us.toString());
-      we(_us);
-    }
-    if (msg is Pos) {
-      // Consider caching pos & course & speed,and then invoking us on these?  RMC is probably the exact same thing, I suspect.
-      // print(msg.runtimeType.toString() +' '+ dms(msg.lat*60, 'N','S',null, 4) +' '+ dms(msg.lng*60, 'E','W',null,4) );
-    }
-    if (msg is VTG) {
-      // print("vtg");
-    }
-
     // Here's the meat:
     if (msg is VDM) {
       // NMEA process has already unpacked the message; the interesting bit is in [payload].
@@ -126,21 +91,17 @@ abstract class AISHandler {
 
         } else if (ais is Type18) {
           _stash(ais.mmsi, 0x18, ais);
-          if (_us != null) {
-            if (ais.course != null) {
-              PCS them = PCS(ais.lat / 60, ais.lon / 60, ais.course, ais.speed);
-              they(_us, them, ais.mmsi);
-            }
+          if (ais.course != null) {
+            PCS them = PCS(ais.lat / 60, ais.lon / 60, ais.course, ais.speed);
+            they(them, ais.mmsi);
           }
 
         } else if (ais is CNB) {
           _stash(ais.mmsi, ais.type, ais);
           // Type 1, 2, 3 extend CNB:
-          if (_us != null) {
-            if (ais.course != null) {
-              PCS them = PCS(ais.lat / 60, ais.lon / 60, ais.course, ais.sog);
-              they(_us, them, ais.mmsi);
-            }
+          if (ais.course != null) {
+            PCS them = PCS(ais.lat / 60, ais.lon / 60, ais.course, ais.sog);
+            they(them, ais.mmsi);
           }
         } else if (ais is Type24B) {
           _stash(ais.mmsi, 0x24B, ais);
@@ -150,10 +111,9 @@ abstract class AISHandler {
           // static stuff: buoys, lanbys etc
           nameFor(ais.mmsi, ais.name);
           _stash(ais.mmsi, 21, ais);
-          // _names[ais.mmsi] = ais.name;
 
           PCS them = PCS(ais.lat / 60, ais.lon / 60, 0, 0);
-          they(_us, them, ais.mmsi);
+          they(them, ais.mmsi);
 
         } else {
           // print("not handled: $ais");
--- test/test3.dart
+++
@@ -1,9 +0,0 @@
-
-import 'package:ais/geom.dart';
-
-int main(var args) {
-  final double alat = 50.1, alon = -1.3, blat = 50.4, blon = -1.6;
-  print(range(alat,alon,blat,blon).toStringAsFixed(1));
-  print(bearing(alat,alon,blat,blon).toStringAsFixed(1));
-  return 0;
-}
